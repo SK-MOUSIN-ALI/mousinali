ad9304bb60f512f5d00395a938b56bdb26b43ceb
Mousin <work.mousin@gmail.com>
2025-12-08 15:58:23 +0530

    Updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -136,7 +136,7 @@ export default function Home() {
               <div className="w-1/2 text-center hero-text">
                 <h5 className="italic text-[#2B2B2B] font-weight-300">Hi It’s Mousin</h5>
                 <h1 className="font-weight-400 capitalize">
-                  UXXX Designer crafting intuitive digital experiences
+                  UX Designer crafting intuitive digital experiences
                 </h1>
                 <p className="text-[#242424] font-weight-300">
                   Focused on clean design, user psychology, and impactful interactions for
